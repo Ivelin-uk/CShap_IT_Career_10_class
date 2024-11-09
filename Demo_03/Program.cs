f88a506847b180bb5e317471a4e869e62243f685
Ivelin-uk <ivoagaininwow@gmail.com>
2024-11-09 12:03:22 +0200

    add

---
+++ Demo_03/Program.cs
@@ -0,0 +1,13 @@
+﻿//string name = "Ivan";
+//Console.WriteLine(name);
+//string numberOne = "10";
+//string numberTwo = "10";
+//string sum = numberOne + numberTwo;
+//Console.WriteLine(sum);
+
+//string firstName = "Ivelin";
+//string lastName = "Mihaylov";
+//string fullName = firstName + " " + lastName;
+//Console.WriteLine(fullName);
+
+
---
+++ Demo_05/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 10;
+Console.WriteLine(a);
+a = a + 5;
+Console.WriteLine(a);
+a += 5;
+Console.WriteLine(a);
+a -= 10;
+Console.WriteLine(a);
+a *= 2;
+a /= 2;
+Console.WriteLine(a);
